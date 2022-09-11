60a9f1119ee02ccf224e001b9818fc9582e24629
RonzhinLeonid <jiehuh87@yandex.ru>
2022-09-11 22:01:52 +0300

    Les2

---
+++ SOAP_REST_gRPC/PumpService/PumpService.svc.cs
@@ -0,0 +1,45 @@
+﻿using PumpService.Interface;
+using PumpService.Service;
+using System.ServiceModel;
+
+namespace PumpService
+{
+    // ПРИМЕЧАНИЕ. Команду "Переименовать" в меню "Рефакторинг" можно использовать для одновременного изменения имени класса "Service1" в коде, SVC-файле и файле конфигурации.
+    // ПРИМЕЧАНИЕ. Чтобы запустить клиент проверки WCF для тестирования службы, выберите элементы Service1.svc или Service1.svc.cs в обозревателе решений и начните отладку.
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.PerSession)]
+    public class PumpService : IPumpService
+    {
+        private readonly IStatisticsService _statisticsService;
+        private readonly ISettingsService _serviceSettings;
+        private readonly IScriptService _scriptService;
+
+        public PumpService()
+        {
+            _statisticsService = new StatisticsService();
+            _serviceSettings = new SettingsService();
+            _scriptService = new ScriptService(Callback, _serviceSettings, _statisticsService);
+        }
+
+        public void RunScript()
+        {
+            _scriptService.Run(10);
+        }
+
+        public void UpdateAndCompileScript(string fileName)
+        {
+            _serviceSettings.FileName = fileName;
+            _scriptService.Compile();
+        }
+
+        IPumpServiceCallback Callback
+        {
+            get
+            {
+                if (OperationContext.Current != null)
+                    return OperationContext.Current.GetCallbackChannel<IPumpServiceCallback>();
+                else
+                    return null;
+            }
+        }
+    }
+}
