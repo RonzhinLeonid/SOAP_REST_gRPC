9835b42600a488ede03a2afc693b537cb7d791c5
RonzhinLeonid <jiehuh87@yandex.ru>
2022-09-16 00:21:46 +0300

    Les3

---
+++ ClinicClient/Program.cs
@@ -0,0 +1,84 @@
+﻿using Google.Protobuf.WellKnownTypes;
+using Grpc.Net.Client;
+using System.Data;
+using static ClinicServiceProtos.ClinicClientService;
+using static ConsultationServiceProtos.ConsultationClientService;
+using static PetServiceProtos.PetClientService;
+
+namespace ClinicClient
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            AppContext.SetSwitch(
+                "System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
+            var channel = GrpcChannel.ForAddress("http://localhost:5001");
+
+
+            ClinicClientServiceClient client = new ClinicClientServiceClient(channel);
+
+            PetClientServiceClient pet = new PetClientServiceClient(channel);
+
+            ConsultationClientServiceClient consultation = new ConsultationClientServiceClient(channel);
+
+            var createClientResponse = client.CreateClient(new ClinicServiceProtos.CreateClientRequest
+            {
+                Document = "PASS123",
+                FirstName = "cтаниcлав",
+                Surname = "Байраковcкий",
+                Patronymic = "Антонович"
+            });
+
+            Console.WriteLine($"Client ({createClientResponse.ClientId}) created successfully.");
+
+            var getClientsResponse = client.GetClients(new ClinicServiceProtos.GetClientsRequest());
+
+            Console.WriteLine("Clients:");
+            Console.WriteLine("========\n");
+            foreach (var clientObj in getClientsResponse.Clients)
+            {
+                Console.WriteLine($"{clientObj.Document} >> {clientObj.Surname} {clientObj.FirstName}");
+            }
+
+            var createPetResponse = pet.CreatePet(new PetServiceProtos.CreatePetRequest
+            {
+                Name = "Питомец",
+                ClientId = 1,
+                Birthday = Timestamp.FromDateTime(DateTime.UtcNow)
+            });
+            
+            Console.WriteLine($"Pet ({createPetResponse.PetId}) created successfully.");
+
+            var getPetsResponse = pet.GetPets(new PetServiceProtos.GetPetsRequest());
+
+            Console.WriteLine("Pets:");
+            Console.WriteLine("========\n");
+            foreach (var petObj in getPetsResponse.Pets)
+            {
+                Console.WriteLine($"{petObj.Name} >> {petObj.Birthday} {petObj.ClientId}");
+            }
+
+            var createConsultationResponse = consultation.CreateConsultation(new ConsultationServiceProtos.CreateConsultationRequest
+            {
+                ClientId = 1,
+                PetId = 1,
+                ConsultationDate = Timestamp.FromDateTime(DateTime.UtcNow),
+                Description = "Консультация"
+            });
+
+            Console.WriteLine($"Consultation ({createConsultationResponse.ConsultationId}) created successfully.");
+
+            var getConsultationsResponse = consultation.GetConsultations(new ConsultationServiceProtos.GetConsultationsRequest());
+
+            Console.WriteLine("Consultations:");
+            Console.WriteLine("========\n");
+            foreach (var consObj in getConsultationsResponse.Consultations)
+            {
+                Console.WriteLine($"{consObj.ConsultationId} >> {consObj.ClientId} {consObj.PetId} {consObj.ConsultationDate} {consObj.Description} ");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SOAP_REST_gRPC/ClinicService/Services/ClientService/PetClientService.cs
@@ -0,0 +1,67 @@
+﻿using ClinicService.Context;
+using ClinicService.Data;
+using ClinicServiceProtos;
+using Grpc.Core;
+using PetServiceProtos;
+using System;
+using static PetServiceProtos.PetClientService;
+
+namespace ClinicService.Services.ClientService
+{
+    public class PetClientService : PetClientServiceBase
+    {
+        #region Serives
+
+        private readonly ClinicServiceDbContext _dbContext;
+        private readonly ILogger<PetClientService> _logger;
+
+        #endregion
+
+        #region Constructors
+
+        public PetClientService(ClinicServiceDbContext dbContext,
+            ILogger<PetClientService> logger)
+        {
+            _logger = logger;
+            _dbContext = dbContext;
+        }
+
+        #endregion
+
+        public override Task<CreatePetResponse> CreatePet(CreatePetRequest request, ServerCallContext context)
+        {
+            var pet = new Pet
+            {
+                Name = request.Name,
+                Birthday = request.Birthday.ToDateTime(),
+                ClientId = request.ClientId
+        };
+
+            _dbContext.Pets.Add(pet);
+
+            _dbContext.SaveChanges();
+
+            var response = new CreatePetResponse
+            {
+                PetId = pet.PetId
+            };
+
+            return Task.FromResult(response);
+        }
+
+        public override Task<GetPetsResponse> GetPets(GetPetsRequest request, ServerCallContext context)
+        {
+            var response = new GetPetsResponse();
+
+            response.Pets.AddRange(_dbContext.Pets.Select(pet => new PetResponse
+            {
+                PetId = pet.PetId,
+                Name = pet.Name,
+                Birthday = Google.Protobuf.WellKnownTypes.Timestamp.FromDateTime(pet.Birthday),
+                ClientId = pet.ClientId
+            }).ToList());
+
+            return Task.FromResult(response);
+        }
+    }
+}
